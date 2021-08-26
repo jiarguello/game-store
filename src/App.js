6103d6de95f14bfcc83b11514c2f0e17633294b5
jhon <jhonatan.arguello@gmail.com>
2021-08-26 10:51:30 -0300

    Criaçao dos componentes que compoem a primeira pagina, sendo eles header, productslist e footer.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Provider from './context/provider';
 import Home from './pages/home';
 import Checkout from './pages/checkout';
+import GlobalStyle from './globalStyle';
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
+          <GlobalStyle />
           <Route path='/purchase' component={Home} />
           <Route path='/' component={Checkout} />
         </Switch>
